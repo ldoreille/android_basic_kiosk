aabb0023509a0241b2eb0b2f0f69dbd6ebcbb629
ldoreille <laurent@doreille.org>
2017-02-25 21:25:09 +0100

    add corrections

--- app/src/main/java/org/doreille/kiosk/MainActivity.java
+++ app/src/main/java/org/doreille/kiosk/MainActivity.java
@@ -1,13 +1,29 @@
 package org.doreille.kiosk;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.doreille.kiosk.receivers.AdminReceiver;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected DevicePolicyManager mDpm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+            setContentView(R.layout.activity_main);
+
+        ComponentName deviceAdmin = new ComponentName(this, AdminReceiver.class);
+        mDpm = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+
+        if (mDpm.isDeviceOwnerApp(getPackageName())) {
+            mDpm.setLockTaskPackages(deviceAdmin, new String[]{getPackageName()});
+            startLockTask();
+
+        }
     }
 }
---
+++ app/src/main/java/org/doreille/kiosk/receivers/AdminReceiver.java
@@ -0,0 +1,6 @@
+package org.doreille.kiosk.receivers;
+
+import android.app.admin.DeviceAdminReceiver;
+
+public class AdminReceiver extends DeviceAdminReceiver {
+}
